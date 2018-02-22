62d5b55dc31b9adf63fa7fee6b3dfa9cee60c7ff
ankit chandra <ankitchandra1994@gmail.com>
2018-02-22 14:16:19 +0530

    this is my loops program

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppControlStament
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("value of i is = " + i);
+               
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Class6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppControlStament
+{
+    class Class6
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int s = 0;
+            while (i <= 10)
+            {
+                s = s + i;
+                Console.WriteLine("i = " + i);
+                i++;
+
+            }
+            Console.WriteLine("Sum = " + s);
+            Console.Read();
+        }
+    }
+}
